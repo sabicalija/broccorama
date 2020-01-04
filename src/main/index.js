4ba51c665224ae8b6bcacfc5c40f0256cbd2cfc9
Alija Sabic <sabic.alija@gmail.com>
2020-01-04 18:37:11 +0100

    Refactor main to use es6 import statements

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require("electron");
-const updater = require("./updater");
+import { app, BrowserWindow } from "electron";
+import updater from "./updater";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
